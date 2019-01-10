1b1f08aa9cdd1dc3182956afae52f211464db5c4
Hyunhak Woo <hyunhak601@hanmail.net>
2019-01-11 00:48:54 +0900

    Initial Commit

---
+++ Preactice_20190111/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Preactice_20190111
+//
+//  Created by Hyunhak Woo on 11/01/2019.
+//  Copyright © 2019 CottonCandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Preactice_20190111Tests/Preactice_20190111Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Preactice_20190111Tests.swift
+//  Preactice_20190111Tests
+//
+//  Created by Hyunhak Woo on 11/01/2019.
+//  Copyright © 2019 CottonCandy. All rights reserved.
+//
+
+import XCTest
+@testable import Preactice_20190111
+
+class Preactice_20190111Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
